019e942345bb9f129be82eef4e388a20ae9cd6d0
Calpa Liu <calpaliu@gmail.com>
2017-08-17 21:30:22 +0800

    Refactor ProgressBar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ProgressBar extends Component {
-  render() {
-    return (
-      <div className="progress" style={{ marginBottom: 10 }}>
-        <div className="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow={this.props.value} aria-valuemin="0" aria-valuemax="100" style={{ width: `${this.props.value}%` }}>
-          <span className="sr-only">{this.props.value}% Complete</span>
-        </div>
-      </div>
-    );
-  }
-}
+const ProgressBar = ({ value }) => (
+  <div className="progress" style={{ marginBottom: 10 }}>
+    <div className="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100" style={{ width: `${value}%` }}>
+      <span className="sr-only">{value}% Complete</span>
+    </div>
+  </div>
+);
 
 export default ProgressBar;
